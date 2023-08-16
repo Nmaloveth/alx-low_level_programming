3e23ecacf672f68b950680d6b4d5835e012bad6d
Nmaloveth <anoliefoloveth1@gmail.com>
2023-08-16 16:58:58 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - is a function that checks for alphabetic character
+ * @c: c is an ascii character
+ *
+ * Return: 1 (if letter)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
