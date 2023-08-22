25168c2967fd1b7a99a1be455aa6a1a18b2c47c6
Nmaloveth <anoliefoloveth1@gmail.com>
2023-08-22 07:03:58 -0700

    0 reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - This function takes a pointer to an int
- * as parameter and updates the value it points to to 98
+ * as parameter and updates the value it points to to 98:@n
  * Return: void
  */
 
